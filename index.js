7505db20f13d198c9397f8d329aced1969debccd
Renard Lomax <renardlomax1990@gmail.com>
2021-11-10 18:27:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,117 @@
+const api_url = "https://jsonplaceholder.typicode.com"
+
+
+async function getData() { 
+    try {
+    const response1 = await fetch(`${api_url}/users`);
+    const data1 = await response1.json();
+    const response2 = await fetch(`${api_url}/posts`);
+    const data2 = await response2.json();
+    console.log('users',data1);
+    console.log('posts', data2 )
+    const usersDiv = document.querySelector(`#all-users`);
+    let users = data1;
+    users.forEach(user => {
+        
+        const userRow = document.createElement('tr')
+        const userCell = document.createElement('td')
+        const userCellId = document.createElement('td')
+        
+        userCell.onclick = function() {
+            const postDiv = document.querySelector(`#user-posts`);
+            let posts = data2.filter(post => post.userId === user.id);
+            postDiv.innerHTML = ''
+            posts.forEach(post => {
+                const postBody = post.body
+                const postTitle = post.title
+                const userPostTitle = document.createElement('h3')
+                const userPostBody = document.createElement('p')
+                
+                userPostTitle.innerText = 'Title: ' + postTitle
+                userPostBody.innerHTML = 'Body:  ' + postBody
+                postDiv.appendChild(userPostTitle)
+                postDiv.append(userPostBody)
+            })
+        }
+        console.log(user.name)
+        userCellId.innerText = `${user.id}` 
+        userCell.innerText = ` ${user.name}`
+        userRow.appendChild(userCellId)
+        userRow.appendChild(userCell)
+        usersDiv.append(userRow)
+        
+    })
+    if(!response1.ok) {
+        throw new Error(`Failed to fetch data: ${response1.status}`);
+    }
+    } catch (error) {
+    console.log(error); 
+    }
+}
+getData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function getPost() { 
+//     try {
+//     const response = await fetch(`${api_url}/posts`);
+//     const data = await response.json();
+//     console.log(data);
+//     if(!response.ok) {
+//         throw new Error(`Failed to fetch data: ${response.status}`);
+//     }
+//     return data
+//     } catch (error) {
+//     console.log(error); 
+  
+//     }
+// }
+// let userPosts = getPost();
+
+// console.log('user data',userData);
+// let showUsers = users => {
+//     const usersDiv = document.querySelector(`all-users`);
+//     users.forEach(user => {
+//         const userTable = document.createElement('p')
+//         userTable.innerText = `All Users ${user.name}`
+//         usersDiv.append(userTable)
+//     })
+// }
+
+ 
+
+
+
+
